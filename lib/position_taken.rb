8333f55d5991fbae2f3ca3dd432afcf243dd718b
Bhabya  Mishra <mbhabya@gmail.com>
2017-09-16 16:59:26 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
-# code your #position_taken? method here!
+
+def position_taken?(board,index)
+  # input = gets.strip
+  return false if((board[index] == "") || ( board[index] == " ") || (board[index] == nil))
+  else
+     true
+end
